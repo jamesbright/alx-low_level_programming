4338b8427624daf606066c4276b1cfd2352b3a82
James Ononiwu <jamesononiwu@gmail.com>
2022-10-13 12:56:21 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - program prints string
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * a text is printed after the code is executed
+ * no variable is used here.
+ * Return: returns 0 at end of execution
+ */
+int main(void)
+{
+printf("%s \n", "with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
