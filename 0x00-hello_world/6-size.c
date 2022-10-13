557f6ad2e1b83c05bda16d5a0759f339eebe1f10
James Ononiwu <jamesononiwu@gmail.com>
2022-10-13 13:26:44 -0700

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - program prints size of types
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * a text is printed after the code is executed
+ * no variable is used here.
+ * Return: returns 0 at end of execution
+ */
+int main(void)
+{
+    char    a    = 'A';
+    int     b    = 187;
+    long    c    = 1338668.86;
+    long long  d = 1767822.40;
+    float    e   = 678.53f;
+
+    printf("\nSize of a char: %d %s", sizeof(a), byte(s));
+    printf("\nSize of an int: %d %s", sizeof(b), byte(s));
+    printf("\nSize of a long: %d %s", sizeof(c), byte(s));
+    printf("\nSize of a long long: %d %s", sizeof(d), byte(s));
+    printf("\nSize of a float: %d %s", sizeof(f), byte(s));
+
+    return (0);
+}
