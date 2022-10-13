0d6894a8b6b46a465dd7301d11bfa0b0c5101490
James Ononiwu <jamesononiwu@gmail.com>
2022-10-13 14:22:43 -0700

    fwrite

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,8 +11,9 @@
  */
 int main(void)
 {
-static const char qoute[] = "and that piece of art is useful\""
-"- Dora Korpar, 2015-10-19\n";
+static const char qoute[] = "and that piece of art is useful\"";
+static const char qoute_ctd[] = " - Dora Korpar, 2015-10-19\n";
 fwrite(qoute, sizeof(qoute) - 1, 1, stdout);
+fwrite(qoute_ctd, sizeof(qoute_ctd) - 1, 1, stdout);
 return (1);
 }
