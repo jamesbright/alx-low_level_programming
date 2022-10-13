95213250752ebc3575cdd17a13ce0bcb2f8e8ae0
James Ononiwu <jamesononiwu@gmail.com>
2022-10-13 12:48:52 -0700

    betty correct

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-
-/*
+/**
  * main - program prints string
- (* a blank line
- * Description: this is an example on the use of puts)
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * a text is printed after the code is executed
+ * no variable is used here.
  * Return: returns 0 at end of execution
  */
 int main(void)
