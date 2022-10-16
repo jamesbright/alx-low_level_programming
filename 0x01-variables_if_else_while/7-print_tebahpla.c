720029325aae7af86c93c30d05c3fc406caa91d8
James Ononiwu <jamesononiwu@gmail.com>
2022-10-16 11:00:10 -0700

    print alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - program prints string and returns 0
+(* a blank line
+ * Description: prints letters of the alphabet)n reverse order)?
+ * a text is printed after the code is executed
+ * Return: returns 0 at end of execution
+ */
+int main(void)
+{
+unsigned int i;
+char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+for (i = strlen(alphabet); i >= 0; i--)
+{
+putchar(alphabet[i]);
+}
+putchar('\n');
+return (0);
+}
