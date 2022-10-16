e4c4672da4fd6a39a7e54de027e91ddbf6bd8053
James Ononiwu <jamesononiwu@gmail.com>
2022-10-16 11:46:33 -0700

    possible combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - program prints string and returns 0
+(* a blank line
+ * Description: prints combination of numbers)?
+ * a text is printed after the code is executed
+ * Return: returns 0 at end of execution
+ */
+int main(void)
+{
+int i,j;
+for (i = 0; i <= 9; ++i)
+{
+for (j = 0 j <= i; ++j)
+{
+putchar('0' + i);
+putchar('0' + j);
+}
+putchar(',');
+}
+putchar('\n');
+return (0);
+}
