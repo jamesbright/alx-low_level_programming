beb561050d27436ccf214ffb5875510d134f5b2a
James Ononiwu <jamesononiwu@gmail.com>
2022-10-16 12:53:33 -0700

    number combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - program prints string and returns 0
+(* a blank line
+ * Description: prints combination of numbers)?
+ * a text is printed after the code is executed
+ * Return: returns 0 at end of execution
+ */
+int main(void)
+{
+int i, j, n = 0;
+for (i = 0; i < 100; i++, n++)
+{
+for (j = i;;)
+{
+if (n > 0)
+{
+if (j / 10 % 10 > j % 10)
+break;
+int k = j /= 10;
+if (k == 0)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('0' + i);
+putchar('0' + j);
+}
+}
+putchar('\n');
+return (0);
+}
