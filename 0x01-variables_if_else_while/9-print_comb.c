1605723d2a68588e886cdc9b352b3d3f9d65b4d6
James Ononiwu <jamesononiwu@gmail.com>
2022-10-16 11:51:47 -0700

    possible combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,15 +10,11 @@
  */
 int main(void)
 {
-int i,j;
+int i;
 for (i = 0; i <= 9; ++i)
 {
-for (j = 0; j <= i; ++j)
-{
 putchar('0' + i);
-putchar('0' + j);
-}
-putchar(',');
+putchar(', ');
 }
 putchar('\n');
 return (0);
