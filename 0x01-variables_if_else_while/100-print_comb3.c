93eb2ef5ad4f53ca3c2f0528a3eae0d9f40c4d51
James Ononiwu <jamesononiwu@gmail.com>
2022-10-17 09:11:17 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,12 +15,12 @@ for (i = 0; i <= 9; ++i)
 {
 for (j = 0; j <= 9; ++j)
 {
-if (i == 9)
-continue;
 if(i < j)
 {
 putchar((i % 10) + '0');
 putchar((j % 10) + '0');
+if(j == 9)
+continue;
 putchar(',');
 putchar(' ');
 }
