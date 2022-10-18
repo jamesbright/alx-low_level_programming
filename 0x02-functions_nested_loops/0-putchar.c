0adb422fa99c5a44648b2740eed192ffa6fab92c
James Ononiwu <jamesononiwu@gmail.com>
2022-10-18 12:03:12 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/** 
+ * main - function to print _putchar
+ * Description: this function uses the prototype of
+ * a function.
+ * Return: always return 0 at end of execution
+ */
+
+int main(void)
+{
+
+printf("%s", "_putchar");
+
+return (0);
+}
