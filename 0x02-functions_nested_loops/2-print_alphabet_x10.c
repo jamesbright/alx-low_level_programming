ba8e3f3a12f69809ffd747a63ff8745e90fa47e5
James Ononiwu <jamesononiwu@gmail.com>
2022-10-19 04:30:58 -0700

    "update"

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-/*** 
+/**
  * print_alphabet_x10 - prints the alphabets 10 times
  * Description: function prints the alphabet 10 times
  *  Return: always return 0 after execution
