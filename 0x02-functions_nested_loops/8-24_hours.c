0d4b46f26314e90d16c3ec37968f9521dd4f202f
James Ononiwu <jamesononiwu@gmail.com>
2022-10-19 07:06:17 -0700

    "update"

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,8 +10,25 @@ void jack_bauer(void)
 {
 int i = 0, j = 0, k = 0, m = 0;
 
-for (; m <= 9; m++)
+for (; m <= 9; ++m)
 {
+
+if (m > 9)
+{
+m = 0;
+k++;
+}
+if (k > 5)
+{
+k = 0;
+j++;
+}
+
+if (j > 9)
+{
+j = 0;
+i++;
+}
 _putchar('0' + i);
 _putchar('0' + j);
 _putchar(':');
@@ -19,24 +36,8 @@ _putchar('0' + k);
 _putchar('0' + m);
 _putchar('\n');
 
-if (m == 9)
-{
-m = 0;
-k++;
-}
-if (k == 5)
-{
-k = 0;
-j++;
-}
-
-if (j == 9)
-{
-j = 0;
-i++;
-}
-
 if (i == 2 && j == 3 && k == 5 && m == 9)
 break;
+
 }
 }
