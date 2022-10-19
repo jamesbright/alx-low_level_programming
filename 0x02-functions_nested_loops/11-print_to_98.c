070ebcf6307ddf4fcf0c8074231003d95cce5792
James Ononiwu <jamesononiwu@gmail.com>
2022-10-19 11:30:11 -0700

    "update"

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,19 @@ void print_to_98(int n)
 int i;
 for (i = n; i <= 98; i++)
 {
-_putchar(i);
+if(i >= 10)
+{
+int digit, lastDigit, firstDigit;
+lastDigit = i % 10;
+digit    = (int)log10(i);    
+firstDigit = (int) (i / pow(10, digit));
+_putchar(firstDigit + '0');
+_putchar(lastDigit + '0');
+}
+else
+{
+_putchar(i + '0');
+}
 if (i == 98)
 continue;
 _putchar(',');
