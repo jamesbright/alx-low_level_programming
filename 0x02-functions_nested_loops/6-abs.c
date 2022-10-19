6e53bc60754580f58d1df5a83e88f09469180ccd
James Ononiwu <jamesononiwu@gmail.com>
2022-10-19 05:40:34 -0700

    "update"

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,6 +3,7 @@
 /**
  * _abs - finds absolute value of a number
  * Description: function finds and prints the absolute value of a number
+ * @n: number argument to find
  * Return: returns the absolute value of a number
  */
 int _abs(int n)
