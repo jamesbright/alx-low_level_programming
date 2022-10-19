59df501a4c64d988cb614dfdef2c737056797054
James Ononiwu <jamesononiwu@gmail.com>
2022-10-19 09:15:01 -0700

    "update"

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdio.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -8,5 +9,23 @@
  */
 int _putchar(char c)
 {
-return (write(1, &c, 1));
+//return (write(1, &c, 1));
+/* print '-' for negative numbers */
+    if (c < 0) {
+        putchar('-');
+        c = c * -1;
+    }
+  
+    /* Print Zero */
+    if (c == 0)
+        putchar('0');
+  
+    /* First remove the last digit of number and print 
+    the remaining digits using recursion, then print
+    the last digit
+ */
+    if (c/10)
+        _putchar(c/10);
+  
+    putchar(c%10 + '0');
 }
