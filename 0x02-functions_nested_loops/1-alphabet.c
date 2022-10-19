0326d66c8ba461dd9fafbad82ba7f5455f997e3c
James Ononiwu <jamesononiwu@gmail.com>
2022-10-19 02:42:44 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function to print alphabets
+ * Description: this function uses the prototype of
+ * a function.
+ * Return: always return 0 at end of execution
+ */
+
+int main(void)
+{
+print_alphabet();
+return (0);
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,51 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * print_alphabet_x10 - prints a string
+ * Description: function to display string
+ * Return: always return 0 after execution
+ */
 void print_putchar(void)
 {
 printf("%s", "_putchar\n");
+}
+
+
+/**
+ * print_alphabet_x10 - prints the alphabets y
+ * Description: function prints the alphabet
+ * Return: always return 0 after execution
+ */
+void print_alphabet(void)
+{
+unsigned int i;
+char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+for (i = 0; i < strlen(alphabet); ++i)
+{
+putchar(alphabet[i]);
+}
+putchar('\n');
+return (0);
+}
+
+/**
+ * print_alphabet_x10 - prints the alphabets 10 times
+ * Description: function prints the alphabet 10 times
+ * Return: always return 0 after execution
+ */
+void print_alphabet_x10(void)
+{
+unsigned int i, counter = 10;
+char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+while (counter > 0)
+{
+for (i = 0; i < strlen(alphabet); ++i)
+{
+putchar(alphabet[i]);
+}
+counter--;
+putchar('\n');
+return (0);
 }
 #endif
