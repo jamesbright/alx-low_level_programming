c774b6c6d3cbb3d7367bee593d50469c4b2ae283
James Ononiwu <jamesononiwu@gmail.com>
2022-10-19 10:47:14 -0700

    "update"

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,20 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 
-void my_putchar(long var) {
-
-    /* print '-' for negative numbers */
-    if (var < 0) {
-        putchar('-');
-        var = var * -1;
-    }
-    
-    if (var/10)
-        my_putchar(var/10);
-
-    putchar(var%10 + '0');
-}
-
 void times_table(void)
 {
 int num = 0, result, i, j;
@@ -24,7 +10,7 @@ putchar('\n');
 for (j = 0; j <= 9; ++j)
 {
 result = num * j;
-printf("%-11%d",result);
+__putchar(result);
 
 if (j == 9)
 continue;
