606342bb9c2760ab3b346768530a5aa18d20afe7
James Ononiwu <jamesononiwu@gmail.com>
2022-10-19 12:35:10 -0700

    "update"

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,10 +9,10 @@
 void times_table(void)
 {
 int num = 0, result, i, j;
-for (i = 0; i <= 9; ++i)
+for (i = 0; i <= 9; i++)
 {
 
-for (j = 0; j <= 9; ++j)
+for (j = 0; j <= 9; j++)
 {
 
 result = num * j;
