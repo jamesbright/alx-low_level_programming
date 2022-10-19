4aac032b36adb15b481a93a3e91b260b8bd468c2
James Ononiwu <jamesononiwu@gmail.com>
2022-10-19 08:38:15 -0700

    "times tablw"

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+void times_table(void)
+{
+int num = 0, result, i, j;
+for(i = 0; i <= 9; ++i)
+{
+_putchar('\n');
+for (j = 0; j <= 9; ++j)
+{
+result = num * j;
+_putchar(result);
+
+if (j == 9)
+continue;
+_putchar(',');
+_putchar(' ');
+}
+num++;
+}
+}
