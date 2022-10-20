090478dc59d8eb94ea1cc40e9f89d89f455875f8
James Ononiwu <jamesononiwu@gmail.com>
2022-10-20 10:41:53 -0700

    update

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * main - finds the multiples of a number
+ * Description: finds and prints multiples of numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0)
+{
+printf(" %s ", "Fizz");
+}
+else if (i % 5 == 0)
+{
+printf(" %s", "Buzz");
+}
+else if (i % 3 == 0 && i % 5 == 0)
+{
+printf(" %s", "FizzBuzz");
+}
+else
+{
+if (i == 1)
+printf("%d", i)
+else
+printf(" %d", i);
+}
+}
+printf("%s","\n");
+return (0);
+}
