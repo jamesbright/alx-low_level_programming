c923a1d1ba7266235d1b0cf5bce945e706fd93a8
James Ononiwu <jamesononiwu@gmail.com>
2022-10-20 03:59:04 -0700

    "update"

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,14 +2,15 @@
 
 /**
 * main - tests function that prints if integer is positive or negative
-* Description: checks if a passed argument is positive or negative
 * Return: 0
 */
 
 int main(void)
 {
 int i;
+
 i = 0;
 positive_or_negative(i);
+
 return (0);
 }
