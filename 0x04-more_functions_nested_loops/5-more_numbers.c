33c0d5b34d77dc412be7e46677f90efd8dfd2cf4
James Ononiwu <jamesononiwu@gmail.com>
2022-10-20 08:36:04 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - function to print a range of numbers
+ * Description: prints a range of numbers from 0 - 14
+ */
+
+void more_numbers(void)
+{
+int i, j;
+for (i = 1; i <= 10; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+
+_putchar(j)
+
+}
+_putchar('\n');
+}
+}
