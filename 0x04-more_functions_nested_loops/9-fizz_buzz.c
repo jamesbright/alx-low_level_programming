224534452a102d5ff27fcac7d52317af52d8c115
James Ononiwu <jamesononiwu@gmail.com>
2022-10-20 11:07:26 -0700

    update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,26 +12,19 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0)
-{
-printf("%s ", "Fizz");
-}
-else if (i % 5 == 0)
-{
-printf("%s", "Buzz");
-}
-else if (i % 3 == 0 && i % 5 == 0)
-{
-printf("%s", "FizzBuzz");
-}
+if (i%15 == 0)
+printf("FizzBuzz\t");
+
+else if ((i%3) == 0)
+printf("Fizz\t");
+
+else if (i%5 == 0)
+printf("Buzz\t");
+
 else
-{
-printf("%d", i);
+printf("%d\t", i);
+
 }
-if (i == 100)
-continue;
-printf(" ");
-}
-printf("%s", "\n");
+
 return (0);
 }
