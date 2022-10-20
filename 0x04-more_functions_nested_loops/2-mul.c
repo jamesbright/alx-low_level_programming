020e29a290143b741854e34c7b04cee95b1a5f06
James Ononiwu <jamesononiwu@gmail.com>
2022-10-20 07:42:14 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+ * mul - multiples two numbers
+ * Description: function multiplies two numbers and returns the result
+ * @c: an argument
+ * @b: another argument
+ * Return: return the product of @c * @b
+ */
+
+int mul(int a, int b)
+{
+int result = a * b;
+return (result);
+}
