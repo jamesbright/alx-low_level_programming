74c9a40a4050fe84902a736458f0f93ddd5edc36
James Ononiwu <jamesononiwu@gmail.com>
2022-10-20 08:08:09 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints  a range of numbers
+ * Description: function prints a range of numbers
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+_putchar('0' + i);
+
+_putchar('\n');
+}
