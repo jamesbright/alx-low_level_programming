f90d69be846d21d55650c635b42042ab0a91ab84
James Ononiwu <jamesononiwu@gmail.com>
2022-10-20 12:33:54 -0700

    update

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle
+ * Description: function prints triangle in a certain order
+ * @size: size of triangle to print
+ */
+
+void print_triangle(int size)
+{
+int i, j;
+if (size < 1)
+{
+_putchar('\n');
+}
+else
+{
+i = 0, j = 0;
+while (i < size)
+{
+while (j < size)
+{
+if (j < size - i - 1)
+_putchar(' ');
+else
+_putchar('#');
+j++;
+}
+_putchar('\n');
+i++;
+}
+}
+}
