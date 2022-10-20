1f9f50e24dd0b8ba5e4280bb8f6d9f0b995bc672
James Ononiwu <jamesononiwu@gmail.com>
2022-10-20 05:12:40 -0700

    update

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,16 +15,14 @@ int largest;
 if (a > b && a > c)
 {
 largest = a;
+return (largest);
 }
-else if (b > a && b > c)
+if (b > a && b > c)
 {
 largest = b;
+return (largest);
 }
-else if (c > a && c > b)
-{
+
 largest = c;
+return (largest);
 }
-
-    return (largest);
-}
-
