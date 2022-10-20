e9a851937a82334ac33902d876f4aaff7fea18d1
James Ononiwu <jamesononiwu@gmail.com>
2022-10-20 07:31:12 -0700

    _isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isupper - checks if a character is upper case
+ * Description: checks and returns true or false if a given character
+ * is positive or negative
+ * Return: true or false
+ */
+
+int _isupper(int c)
+{
+if (isupper(c))
+return (1);
+else
+return (0);
+}
