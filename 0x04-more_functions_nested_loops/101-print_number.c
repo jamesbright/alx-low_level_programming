f2e2d1998e43ef2c2fa0387addc24d1e1d9d9329
James Ononiwu <jamesononiwu@gmail.com>
2022-10-21 07:25:42 -0700

    update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,16 +10,17 @@ void print_number(int n)
 {
 if (n / 10 != 0)
 {
-_putchar(n % 10);
-print_number((n - n % 10) / 10);
-}
-else if ((n / 10 == 0) && (n % 10 != 0) && (n > 0))
-{
-_putchar(n % 10 + '0');
-}
-else if ((n / 10 == 0) && (n % 10 != 0) && (n <= 0))
+if (n < 0)
 {
 _putchar('-');
-_putchar(-n % 10 + '0');
+n = -n;
+}
+if (n == 0)
+_putchar('0');
+
+if (n / 10)
+print_number(n / 10);
+
+_putchar(n % 10 + '0');
 }
 }
