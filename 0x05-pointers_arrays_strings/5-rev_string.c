16e920f32b046a03a47d79c3749456eaed9f9707
James Ononiwu <jamesononiwu@gmail.com>
2022-10-24 11:19:40 -0700

    update

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,17 +9,17 @@
 void rev_string(char *s)
 {
 int i = 0, j = 0;
-char str[500];
+char str1[500];
 
 while (*(s + i))
 {
-*(str + i) = *(s + i);
+*(str1 + i) = *(s + i);
 i++;
 }
 i = i - 1;
 while (i >= 0)
 {
-*(s + i) = *(str + j);
+*(s + i) = *(str1 + j);
 j++;
 i--;
 }
