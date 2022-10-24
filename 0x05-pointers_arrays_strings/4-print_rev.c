314f7c906e0d54bb9e273da70596514e399228eb
James Ononiwu <jamesononiwu@gmail.com>
2022-10-24 07:43:31 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - function reverses string 
+ * Description: function reverses string to another
+ * @s: string to be reversed
+ */
+void print_rev(char *s)
+{
+int i, temp, len = strlen(s);
+for (i = 0; i < len/2; i++)
+{
+temp = *s[i];
+*s[i] = *s[len - i - 1];
+*s[len -i - 1] = temp;
+}
+puts(s);
+}
