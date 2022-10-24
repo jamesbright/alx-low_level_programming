2d31d88432c5452dee1d6d2cb202b05fcf5fad31
James Ononiwu <jamesononiwu@gmail.com>
2022-10-24 11:45:53 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts - function to print character
+ * @str: variable
+ * Description: function to print all characters in a
+ */
+
+void puts(char *str)
+{
+int i = 0;
+while (*(s + i))
+{
+
+if (i == 0)
+_putchar(*(s + i));
+
+_putchar(*(s + i + 1));
+
+}
+}
