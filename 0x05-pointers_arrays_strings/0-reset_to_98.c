68501f3c3008d872e63d2155fc9c6da189bbb6dd
James Ononiwu <jamesononiwu@gmail.com>
2022-10-24 06:34:31 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function changes value through a pointer
+ * Description: this function changes the value of a variable through a pointer
+ * @n: pointer pointing to the passed value
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
