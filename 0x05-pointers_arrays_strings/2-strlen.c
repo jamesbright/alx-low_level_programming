80c5814ecb0686ef180fedf33c3e5cfa941274f7
James Ononiwu <jamesononiwu@gmail.com>
2022-10-24 06:57:22 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - returns length of a string
+ * Description: counts and returns the length of a string
+ * @s: string to be returned
+ * Return: length of s
+ */
+
+int _strlen(char *s)
+{
+return (strlen(*s));
+}
