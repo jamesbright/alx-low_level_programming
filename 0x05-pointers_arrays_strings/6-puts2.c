de0ab3199f720f05e07c38280836b40ab70201e4
James Ononiwu <jamesononiwu@gmail.com>
2022-10-24 12:22:03 -0700

    update

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,21 +1,18 @@
 #include "main.h"
 
 /**
- * puts - function to print character
+ * puts2 - function to print character
  * @str: variable
  * Description: function to print all characters in a
  */
 
-void puts(char *str)
+void puts2(char *str)
 {
 int i = 0;
-while (*(s + i))
+while (*(str + i))
 {
-
-if (i == 0)
-_putchar(*(s + i));
-
-_putchar(*(s + i + 1));
-
+_putchar(*(str + i));
+i = i + 2;
 }
+_putchar('\n');
 }
