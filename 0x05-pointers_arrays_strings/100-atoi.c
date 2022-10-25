a87667648aa85323b5e61c82ee13ec56a0e00b14
James Ononiwu <jamesononiwu@gmail.com>
2022-10-25 12:31:02 -0700

    update

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,9 +11,7 @@
 int _atoi(char *s)
 {
 int i = 0, sign = 1, num = 0, result;
-while ((s[i] == ' ' || s[i] == '-' ||
-s[i] == '+') && !(s[i + 1] >= '0'
-&& s[i + 1] <= '9'))
+while (s[i] == ' ' || s[i] == '-' || s[i] == '+')
 {
 i++;
 }
