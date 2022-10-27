1a5c0ad0c977c66f1e130da095c654269fa2263a
James Ononiwu <jamesononiwu@gmail.com>
2022-10-27 04:43:05 -0700

    update

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,8 +12,8 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-int i, dest_len = strlen(dest) + 1;
-for (i = 0; i < n; i++)
+int i, dest_len = strlen(dest);
+for (i = 0; i < n && src[i] != '\0'; i++)
 dest[dest_len + i] = src[i];
 dest[dest_len + i] = '\0';
 
