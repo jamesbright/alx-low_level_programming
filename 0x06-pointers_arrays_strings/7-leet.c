c2a5a1f2ec049665f8461213f01413694e0259be
James Ononiwu <jamesononiwu@gmail.com>
2022-10-27 13:44:38 -0700

    update

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,6 +12,8 @@ char *leet(char *str)
 char l[] = {'a', 'e', 'o', 't', 'l'};
 char n[] = {4, 3, 0, 7, 1};
 int i;
+char *r = str;
+
 while (*str)
 {
 for (i = 0; i < 5; i++)
@@ -21,5 +23,5 @@ if (*str == l[i] || *str == l[i] - 32)
 }
 str++;
 }
-return (str);
+return (r);
 }
