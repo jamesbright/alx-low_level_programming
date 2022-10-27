ad8a141a81fbc1df2c5fea22631949d0353a5b1c
James Ononiwu <jamesononiwu@gmail.com>
2022-10-27 04:30:59 -0700

    strncat implementation

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - concatenates two strings
+ * Description: does not consider src string to be null terminated
+ * @dest: destination string
+ * @src: source string
+ * @n: n bytes of src string
+ * Return: concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int i, dest_len = strlen(dest);
+for (i = 0; i < n; i++)
+{
+dest[dest_len + i] = src[i];
+}
+return (dest);
+}
