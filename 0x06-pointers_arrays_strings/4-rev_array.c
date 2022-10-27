5a564adf700378f29d522d4e016fca23122cf84b
James Ononiwu <jamesononiwu@gmail.com>
2022-10-27 14:27:43 -0700

    update on rev_string

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,16 +10,26 @@
 
 void reverse_array(int *a, int n)
 {
-int i = n - 1;
-while (i >= 0)
+int temp, i;
+int *first = a;
+int *last = a + n - 1;
+while (first < last)
 {
-printf("%d", a[i]);
+temp = *first;
+*first = *last;
+*last = temp;
+first++;
+last--;
+}
+for (i = 0; i < n; i++)
+{
+
 if (i > 0)
 {
 printf(", ");
 }
+printf("%d", a[i]);
 
-i--;
 }
-
+printf("\n");
 }
