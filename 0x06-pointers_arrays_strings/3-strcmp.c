2ee3159e04af111df251cf3aff72edb4edc723bd
James Ononiwu <jamesononiwu@gmail.com>
2022-10-27 06:16:28 -0700

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,20 +12,21 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int i;
-for (i = 0; s1[i] && s2[i]; ++i)
+int result = 0;
+while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 {
-if (s1[i] == s2[i] || (s1[i] ^ 32) == s2[i])
-continue;
-else
-break;
+s1++;
+s2++;
 }
 
-if (s1[i] == s2[i])
-return (0);
+if (*s1 == *s2)
+result = 0;
 
-if ((s1[i] | 32) < (s2[i] | 32))
-return (-15);
+else if (*s1 < *s2)
+result = -15;
 
-return (15);
+else
+result = 15;
+
+return (result);
 }
