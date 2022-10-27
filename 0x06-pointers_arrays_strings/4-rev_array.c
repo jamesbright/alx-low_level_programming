08e2f007f41cd9dd8f781080bd2d0d0b306a6809
James Ononiwu <jamesononiwu@gmail.com>
2022-10-27 06:48:41 -0700

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - reverses an array
+ * Description prints an array in a reversed order
+ * @a: array
+ * @n:size of a
+ */
+
+void reverse_array(int *a, int n)
+{
+int i = n - 1;
+while (i >= 0)
+{
+printf("%d", a[i]);
+if (i > 0)
+{
+printf(", ");
+}
+
+i--;
+}
+printf("\n");
+}
