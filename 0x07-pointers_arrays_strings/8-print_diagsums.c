7e3a94a18b6152c27d0f0ffb9496d8d7fa9fb164
James Ononiwu <jamesononiwu@gmail.com>
2022-10-31 13:37:03 -0700

    update

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -15,7 +15,7 @@ int l_sum = 0, r_sum = 0;
 for (i = 0, j = 1; i < size; i++, j++)
 {
 l_sum = l_sum +  a[(size + 1) * i];
-r_sum = r_sum + a[(size - 1) * i + 1];
+r_sum = r_sum + a[(size - 1) * j];
 }
 printf("%d, ", l_sum);
 printf("%d, ", r_sum);
