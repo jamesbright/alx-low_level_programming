06a11602ca0f9b5801c407f843e15e3e865ad37b
James Ononiwu <jamesononiwu@gmail.com>
2022-11-05 14:05:04 -0700

    update

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,16 +11,17 @@
 
 int main(int argc, char *argv[])
 {
-if (argc > 0)
+
+if (argc < 3)
+{
+printf("%s\n", "Error");
+return (1);
+}
+else
 {
 int a = atoi(argv[1]), b = atoi(argv[2]);
 int result = a * b;
 printf("%d\n", result);
-}
-else if (argc < 2)
-{
-printf("%s\n", "Error");
-return (1);
 }
 return (0);
 }
