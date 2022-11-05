c2837ff0506f77da2cbc0342502ecca4ba3fe6a9
James Ononiwu <jamesononiwu@gmail.com>
2022-11-05 12:01:40 -0700

    argc & argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints it's name
+ * Desription: program prints it's name followed by a new line
+ * @argc:  argument count
+ * @argv: arguments passed in
+ * Return: 0
+ */
+
+int main (int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
