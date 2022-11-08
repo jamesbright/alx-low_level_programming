89f720cb77c32a11467063f9cd494e9b08e8cccd
James Ononiwu <jamesononiwu@gmail.com>
2022-11-08 09:37:24 -0800

    update

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,44 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
+
+/**
+ * word_count - function counts number of words
+ * Description: counts and returns number of words contained in a string
+ * @str: string being passed to check for words
+ * Return: number of words
+ */
+int word_count(char *str)
+{
+int a, num = 0;
+
+for (a = 0; str[a] != '\0'; a++)
+{
+if (*str == ' ')
+str++;
+else
+{
+for (; str[a] != ' ' && str[a] != '\0'; a++)
+str++;
+num++;
+}
+}
+return (num);
+}
+
+/**
+ * free_memory - frees the memory
+ * Description: frees memory of passed pointer value
+ * @string: pointer values passed for freeing
+ * @i: count
+ */
+void free_memory(char **string, int i)
+{
+for (; i > 0;)
+free(string[--i]);
+free(string);
+}
+
 /**
  * strtow - splits a string into words
  * Description: each elememt of the array will contain a single word
@@ -11,9 +49,13 @@
 
 char **strtow(char *str)
 {
-int i, j = 0, k = 0, str_len = strlen(str);
+int i, j = 0, k = 0, str_len = word_count(str);
 char **str_words = NULL;
-str_words = malloc(str_len * sizeof(char));
+
+if (str == 0 || str == NULL)
+return (NULL);
+
+str_words = malloc(str_len + 1 * sizeof(char *));
 if (str_words == NULL)
 return (NULL);
 
