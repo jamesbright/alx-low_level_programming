49ecc0d6d24349cc7ce25a03c714eed1aaa5d0ff
James Ononiwu <jamesononiwu@gmail.com>
2022-11-10 13:30:31 -0800

    update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,7 +3,7 @@
 
 /**
  * _calloc - allocates memory for an array
- * desecription: function allocates memory for an array of size 
+ * desecription: function allocates memory for an array of size
  * @nmemb: elements of the array
  * @size: size of the array
  * Return: a pointer to the newly allocated memory
