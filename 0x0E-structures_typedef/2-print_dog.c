6906899025aa10fd74367f8ac1b76b554247ff7f
James Ononiwu <jamesononiwu@gmail.com>
2022-11-14 11:51:37 -0800

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - displays elements of struct dog
+ * @d: instance of struct dog
+ */
+
+void print_dog(struct dog *d)
+{
+if (d != NULL)
+{
+if (d->age == NULL || d->owner == NULL)
+printf("%s", "nil");
+else if (d->name == NULL)
+printf("%s", "Name: (nil)");
+}
+}
