bdda116b0f5c72083d13276910d46b57bcc44e93
James Ononiwu <jamesononiwu@gmail.com>
2022-11-14 11:16:48 -0800

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - a struct that defines a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner property
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
