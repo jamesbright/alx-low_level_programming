6f4cc69807cb8d961de8f16d67a5b469f71c97d1
James Ononiwu <jamesononiwu@gmail.com>
2022-11-14 12:09:08 -0800

    update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,9 +11,14 @@ void print_dog(struct dog *d)
 {
 if (d != NULL)
 {
-if (d->age == NULL || d->owner == NULL)
-printf("%s", "nil");
-else if (d->name == NULL)
-printf("%s", "Name: (nil)");
+if (d->name == NULL)
+printf("Name: %p\n", (void *)d->name);
+else
+printf("Name: %s\n", d->name);
+printf("Age: %f\n", d->age);
+if (d->owner == NULL)
+printf("Owner: %p\n", (void *)d->owner);
+else
+printf("Owner: %s\n", d->owner);
 }
 }
