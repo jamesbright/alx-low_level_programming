df7d778c275fd7c270880099b17ce883998c418b
James Ononiwu <jamesononiwu@gmail.com>
2022-11-14 12:49:55 -0800

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,20 +16,8 @@ ptr = malloc(sizeof(dog_t));
 if (ptr == NULL)
 return (NULL);
 
-if (name == NULL)
-{
-free(ptr);
-free(owner);
-return (NULL);
-}
-if (owner == NULL)
-{
-free(ptr);
-free(name);
-return (NULL);
-}
-
 ptr->name = name;
+ptr->age = age;
 ptr->owner = owner;
 return (ptr);
 }
