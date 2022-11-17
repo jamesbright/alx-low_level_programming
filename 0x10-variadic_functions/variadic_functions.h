5bcf544e92d18751a1b343de8deede2d6edb5d7a
James Ononiwu <jamesononiwu@gmail.com>
2022-11-17 10:05:02 -0800

    print_all first commit

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,6 +8,19 @@ int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);void print_all(const char * const format, ...);
+
+/**
+ * struct fkey - a key for picking functions to use
+ *
+ * @f: function to use for printing
+ * @spec: character specifier
+ */
+typedef struct fkey
+{
+void (*f)(va_list);
+char spec;
+} fkey;
 
 
 #endif /* END _VARIADIC_FUNCTION_H */
