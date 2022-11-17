f055a87a8b023c44dd676a5aec4e7dfe5bed7be2
James Ononiwu <jamesononiwu@gmail.com>
2022-11-17 02:39:32 -0800

    updated 0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,1 +1,10 @@
+#ifndef _FUNCTION_POINTERS_H
+#define _FUNCTION_POINTERS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+
 void print_name(char *name, void (*f)(char *));
+#endif
