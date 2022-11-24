2cb18419021725c8f55ec1773a0e461b9f6a3196
James Ononiwu <jamesononiwu@gmail.com>
2022-11-24 11:52:10 -0800

    print first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void before_main(void)__attribute__((constructor));
+
+/**
+ * before_main - executes before the main function
+ */
+
+void before_main(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
