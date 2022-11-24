6f19d891e381a03f33dfb79f203aa673b8259a76
James Ononiwu <jamesononiwu@gmail.com>
2022-11-24 09:41:43 -0800

    update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -14,9 +14,10 @@ int _putchar(char c);
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
 #endif /* END _LISTS_H */
+
