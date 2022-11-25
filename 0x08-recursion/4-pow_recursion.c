87afd28b773138ea3e8744e5fc82b50b760893f2
James Ononiwu <jamesononiwu@gmail.com>
2022-11-25 05:55:17 -0800

    update

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  * _pow_recursion - returns value of x raised to power y
  * @x: integer raised to y
  * @y: power of x
- * return -1 if y is < 0 otherwise return x^y
+ * Return: return -1 if y is < 0 otherwise return x^y
  */
 int _pow_recursion(int x, int y)
 {
