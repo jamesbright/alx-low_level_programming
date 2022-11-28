9a31d4edf3e8130b1b1e4596b98ecc315218dfb0
James Ononiwu <jamesononiwu@gmail.com>
2022-11-28 01:22:09 -0800

    add node to start

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -21,4 +21,5 @@ struct listint_s *next;
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 #endif /* END _LISTS_H */
