4841f795bab3b68254911fd868f46c4f2d54e001
James Ononiwu <jamesononiwu@gmail.com>
2022-11-29 12:31:54 -0800

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,9 +11,9 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 unsigned int i = 0;
-listint_t *new_node, *tmp, *holder;
+listint_t *new_node, *temp;
 
-if (head == NULL || *head == NULL)
+if (head == NULL)
 return (NULL);
 
 new_node = malloc(sizeof(listint_t));
@@ -21,35 +21,29 @@ if (new_node == NULL)
 return (NULL);
 
 new_node->n = n;
-new_node->next = NULL;
+temp = *head;
 
 if (idx == 0)
 {
 new_node->next = *head;
 *head = new_node;
-return (new_node);
+return (*head);
 }
-holder = *head;
-tmp = (*(head))->next;
 
-while (tmp != NULL)
+
+while (temp != NULL)
 {
-if (i == idx)
+if (i == idx - 1)
 {
-holder->next = new_node;
-new_node->next = tmp;
-return (new_node);
+new_node->next = temp->next;
+temp->next = new_node;
 }
-holder = tmp;
-tmp = tmp->next;
 i++;
+temp = temp->next;
 }
 
-if(tmp == NULL && i == idx)
-{
-holder->next = new_node;
-return (new_node);
-}
-free(new_node);
+if (idx > i)
 return (NULL);
+
+return (new_node);
 }
