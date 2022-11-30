9db049eb8e6c11ad7c937eca2898b20a68b98f83
James Ononiwu <jamesononiwu@gmail.com>
2022-11-30 03:21:41 -0800

    update

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,20 +1,16 @@
 #include "lists.h"
 
 /**
- * reverse_listint - reverses a linked list
- * @head: list to reverse
- * Return: a pointer to the next node of the reversed list
+ * reverse_recur - recursively reverses a listint list
+ *
+ * @first: node to reverse
+ * @second: node after node to reverse
+ *
+ * Return: void
  */
-
-listint_t *reverse_listint(listint_t **head)
+listint_t *reverse_recur(listint_t *first, listint_t *second)
 {
-listint_t *prev, *ptr, *first, *second;
-
-if (head == NULL || *head == NULL)
-return (NULL);
-
-first = *head;
-second = NULL;
+listint_t *ptr, *prev = NULL;
 
 ptr = first;
 while (ptr->next != second)
@@ -30,7 +26,21 @@ first->next = ptr->next;
 if (first != ptr && second != first)
 second = reverse_recur(second, first);
 ptr->next = second;
+return (ptr);
+}
 
-*head = ptr; 
+
+/**
+ * reverse_listint - reverses a linked list
+ * @head: list to reverse
+ * Return: a pointer to the next node of the reversed list
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+if (head == NULL || *head == NULL)
+return (NULL);
+
+*head = reverse_recur(*head, NULL);
 return (*head);
 }
