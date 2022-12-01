a1c302434921c2a8252761a7cf3b57273cc34f6e
James Ononiwu <jamesononiwu@gmail.com>
2022-12-01 04:35:27 -0800

    binary to decimal

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+#endif /* END _MAIN_H */
