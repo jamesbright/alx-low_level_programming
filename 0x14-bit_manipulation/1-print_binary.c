ab61ffbaaeeb12078c7f56cc907937b56d0a31d6
James Ononiwu <jamesononiwu@gmail.com>
2022-12-01 04:49:19 -0800

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: decimal number
+ */
+
+void print_binary(unsigned long int n)
+{
+int i = 0, counter, j, holder;
+
+if (n == 0)
+{
+printf("0");
+return;
+}
+
+holder = n;
+
+while (holder != 0)
+{
+i++;
+holder = holder >> 1;
+}
+
+
+for (counter = i - 1; counter >= 0; counter--)
+{
+j = n >> counter;
+if(j & 1)
+printf("1");
+else
+printf("0");
+}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,4 +7,5 @@
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 #endif /* END _MAIN_H */
