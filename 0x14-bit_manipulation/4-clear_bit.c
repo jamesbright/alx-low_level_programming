0dcaf3657fa1474421a4eb9913a09b0e53a85688
James Ononiwu <jamesononiwu@gmail.com>
2022-12-01 05:43:58 -0800

    hot fix

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -4,6 +4,7 @@
  * clear_bit - sets the value of a bit to 0
  * @n: input
  * @index: index to clear
+ * Return: 1 On success, -1 on failure
  */
 
 int clear_bit(unsigned long int *n, unsigned int index)
@@ -14,6 +15,8 @@ if (n == NULL)
 return (-1);
 
 bit = 1 << index;
+if (bit == NULL)
+return (-1);
 
 if ((bit | *n) == *n)
 *n = *n ^ bit;
