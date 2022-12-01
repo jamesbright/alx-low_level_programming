3600862212c8137ea119e6540179b55cd8373b72
James Ononiwu <jamesononiwu@gmail.com>
2022-12-01 05:54:27 -0800

    flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flip_bits - counts no of times to flip bits
+ * @n: input
+ * @m: bits
+ * Return: 0
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned int counter = 0;
+
+while (n != 0 || m != 0)
+{
+if ((n & 1) != (m & 1))
+counter++;
+m = m >> 1;
+n = n >> 1;
+}
+
+return (count);
+
+}
