453c72fb12cde0e333bde7a910770801d6543ca6
James Ononiwu <jamesononiwu@gmail.com>
2022-12-05 04:15:23 -0800

    implemented function to read text file

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif /* _MAIN_H */
