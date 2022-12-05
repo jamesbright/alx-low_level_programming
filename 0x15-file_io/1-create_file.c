ae08ab3d6ac9c2ec8c0b14d5fdad03edd35d3d3e
James Ononiwu <jamesononiwu@gmail.com>
2022-12-05 05:01:38 -0800

    added create file function

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file.
+ * @filename: name of file
+ * @text_content: content of file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+int char_count = 0, fd;
+
+if (filename == NULL)
+return (-1);
+
+if (text_content == NULL)
+text_content = "";
+
+while (text_content[char_count] != '\0')
+{
+char_count++;
+}
+
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+if (fd == -1)
+return (-1);
+
+write(fd, text_content, char_count);
+
+return (1);
+}
