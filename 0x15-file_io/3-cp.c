c3895f2749b80555303ca635722343c6e7a14540
James Ononiwu <jamesononiwu@gmail.com>
2022-12-05 06:36:52 -0800

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,18 +6,14 @@
 * @argv: string argument
 * Return: 0
 */
-
 int main(int argc, char *argv[])
 {
 int file_from, file_to;
 int num1 = 1024, num2 = 0;
 char buf[1024];
-
 if (argc != 3)
-{
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
-}
+dprintf(STDERR_FILENO,
+"Usage: cp file_from file_to\n"), exit(97);
 file_from = open(argv[1], O_RDONLY);
 if (file_from == -1)
 {
@@ -42,13 +38,10 @@ num2 = write(file_to, buf, num1);
 if (num2 < num1)
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 }
-
 if (close(file_from) == -1)
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
-
 if (close(file_to) == -1)
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
-
 return (0);
 }
 
