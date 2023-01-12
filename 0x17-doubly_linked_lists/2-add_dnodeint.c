34476ae96a1080524c017895d6e7db059bee53c3
James Ononiwu <jamesononiwu@gmail.com>
2023-01-12 11:52:26 -0800

    hot fix

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,10 +15,17 @@ dlistint_t *new_node = malloc(sizeof(dlistint_t));
 if (new_node == NULL)
 return (NULL);
 
+if(temp)
+{
 temp->prev = new_node;
-new_node->n = n;
 new_node->next = temp;
+}
+else
+{
+new_node->next = NULL;
+}
 new_node->prev = NULL;
+new_node->n = n;
 *head = new_node;
 
 return (new_node);
