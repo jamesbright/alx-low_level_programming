ff790eb01f631ae5b5fd5e4298e32ec7353366a9
James Ononiwu <jamesononiwu@gmail.com>
2023-01-12 05:23:49 -0800

    print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all elements of a list
+ * @h: list element
+ * @Return: number of elements
+ */
+
+
+size_t print_dlistint(const dlistint_t *h)
+{
+int i = 0;
+while (h != NULL)
+{
+printf("%d", h->n);
+h = h->next;
+i = i + 1;
+}
+return (i);
+}
