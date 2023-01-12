cd83d8bb8c4bf40e96298beb4c71f72eadb84813
James Ononiwu <jamesononiwu@gmail.com>
2023-01-12 11:26:24 -0800

    corrected while loop

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -10,7 +10,9 @@ size_t dlistint_len(const dlistint_t *h)
 {
 size_t count = 0;
 while (h)
+{
+h = h->next;
 count = count + 1;
-
+}
 return (count);
 }
---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add a node at the begining of a
+ * linked list.
+ * @h: linked list
+ * Return: new list with node added
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+
+}
