27e574857969fb2a9186ad306e871ac437d4abcc
James Ononiwu <jamesononiwu@gmail.com>
2023-01-13 00:53:47 -0800

    free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a linked list
+ * @head: linked list to free
+ */
+
+
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *ptr = head;
+
+while (head)
+{
+head = head->next;
+free(cursor);
+cursor = head;
+}
+}
