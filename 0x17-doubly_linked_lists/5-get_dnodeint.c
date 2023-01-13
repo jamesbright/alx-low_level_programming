479d9f94a97d417dbbcc2bf93fc7ac6daa7321bf
James Ononiwu <jamesononiwu@gmail.com>
2023-01-13 01:11:16 -0800

    node at positionon

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - gets node at an index
+ * @head: linked list to check
+ * @index: index of the node
+ * Return: node at index
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+int i = 0;
+dlistint_t nth_node = NULL;
+while(head && i <= index)
+{
+if (i == index)
+{
+nth_node = head;
+break;
+}
+head = head->next;
+i = i + 1;
+
+return (nth_node);
+}
