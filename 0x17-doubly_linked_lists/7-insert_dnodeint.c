393a81d3827a5f9033e49a0b6b8dec96cf20159a
James Ononiwu <jamesononiwu@gmail.com>
2023-01-13 02:02:16 -0800

    update

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,6 +13,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 unsigned int i = 0;
 dlistint_t *new_node;
 dlistint_t *ptr = *h;
+if (h == NULL)
+return (NULL);
+if (idx == 0)
+return (add_dnodeint(h, n));
+
 new_node = malloc(sizeof(dlistint_t));
 if (new_node == NULL)
 return (NULL);
