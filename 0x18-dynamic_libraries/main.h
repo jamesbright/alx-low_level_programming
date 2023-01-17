9955af5a48db5960fb954e83b87bacb56cf1ef6f
James Ononiwu <jamesononiwu@gmail.com>
2023-01-17 00:58:43 -0800

    updated and tested

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,6 @@
 #ifndef _MAIN_H
 #define _MAIN_H
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -21,7 +22,7 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /** END _MAIN_H **/
+#endif /* END _MAIN_H */
 
 
 
