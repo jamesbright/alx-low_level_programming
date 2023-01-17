4b936ef63e71227c3804bcb6ebcc14bfce2b3227
James Ononiwu <jamesononiwu@gmail.com>
2023-01-17 02:14:07 -0800

    updated

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-wget -P /tmp https://github.com/jamesbright/alx-low_level_programming/blob/master/0x18-dynamic_libraries/gigamillions.so
+wget -P /tmp https://github.com/jamesbright/alx-low_level_programming/raw/master/0x18-dynamic_libraries/gigamillions.so
 export LD_PRLOAD=/tmp/gigamillions.so
