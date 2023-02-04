39facbb866f2f1e2e9bb7e63c874db4457ab5249
James Ononiwu <jamesononiwu@gmail.com>
2023-02-04 05:58:23 -0800

    get value from kry

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -39,4 +39,5 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 int check_collision(hash_node_t *temp, const char *key, const char *value, char *temp_val);
+char *hash_table_get(const hash_table_t *ht, const char *key);
 #endif /* END _HASH_TABLES_H */
