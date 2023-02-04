cf0eec4d80d55ef3ebc2b53c3365546de915aeea
James Ononiwu <jamesononiwu@gmail.com>
2023-02-03 19:22:25 -0800

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,13 +17,17 @@ dprintf(STDERR_FILENO,
 file_from = open(argv[1], O_RDONLY);
 if (file_from == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+dprintf(STDERR_FILENO, 
+"Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+file_to = open(argv[2], O_WRONLY | O_CREAT |
+O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP 
+| S_IWGRP | S_IROTH);
 if (file_to == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+dprintf(STDERR_FILENO, 
+"Error: Can't write to %s\n", argv[2]);
 close(file_from), exit(99);
 }
 while (num1 == 1024)
@@ -31,17 +35,20 @@ while (num1 == 1024)
 num1 = read(file_from, buf, 1024);
 if (num1 == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+dprintf(STDERR_FILENO, 
+"Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
 num2 = write(file_to, buf, num1);
 if (num2 < num1)
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+dprintf(STDERR_FILENO, 
+"Error: Can't write to %s\n", argv[2]), exit(99);
 }
 if (close(file_from) == -1)
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
+file_from), exit(100);
 if (close(file_to) == -1)
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
+dprintf(STDERR_FILENO, "Error: 
+Can't close fd %d\n", file_to), exit(100);
 return (0);
 }
-
