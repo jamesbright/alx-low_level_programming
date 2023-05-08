e6ac8c186e14032d1797eefb2dab789f5d302548
James Ononiwu <jamesononiwu@gmail.com>
2023-05-08 06:24:44 -0700

    fixed betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,7 +14,7 @@ int linear_search(int *array, size_t size, int value)
 {
 size_t i = 0;
 
-while(i < size)
+while (i < size)
 {
 printf("Value checked array[%li] = [%d]\n", i, array[i]);
 if (array[i] == value)
